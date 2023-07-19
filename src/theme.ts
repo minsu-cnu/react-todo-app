efbb6b710fc0164042c21f1c3cd7c10fdf73c165
minsu-cnu <duecost@gmail.com>
2023-07-20 08:40:26 +0900

    setup complete

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+  boxBgColor: "rgba(0, 0, 0, 0.5)",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "#black",
+  accentColor: "#9c88ff",
+  boxBgColor: "rgba(0, 0, 0, 0.1)",
+};
